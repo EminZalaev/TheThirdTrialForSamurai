2673a37cf23fe323ca2ce4567f82a1489d18c3ba
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-20 19:39:47 +0300

    dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +25,56 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func uniqLower(in *[]string) []string {
+	res := make([]string, 0, len(*in))
+	u := make(map[string]bool)
 
+	for _, i := range *in {
+		if !u[i] {
+			res = append(res, strings.ToLower(i))
+			u[i] = true
+		}
+	}
+
+	return res
+}
+
+func anagramDict(in *[]string) *map[string][]string {
+	if len(*in) < 2 {
+		return nil
+	}
+
+	// промежуточная мапа, ключ - отсортированное слов
+	buff := make(map[string][]string)
+
+	uniqIn := uniqLower(in)
+	for _, i := range uniqIn {
+		sorted := []rune(i)
+		sort.Slice(sorted, func(i, j int) bool {
+			return sorted[i] < sorted[j]
+		})
+
+		word := string(sorted)
+		buff[word] = append(buff[word], i)
+
+	}
+
+	fmt.Println(buff)
+
+	res := make(map[string][]string)
+	for _, words := range buff {
+		if len(words) > 1 {
+			sort.Strings(words)
+			res[words[0]] = words
+		}
+	}
+
+	return &res
+}
+
+func main() {
+	input := []string{"тест", "листок", "пятка", "пятак", "тяпка", "листок", "пятка", "слиток", "столик"}
+
+	fmt.Println(input)
+	fmt.Println(anagramDict(&input))
 }
