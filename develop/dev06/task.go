df986ac36995cfefbb06b8d19350e33f55fc2cc6
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-22 20:32:01 +0300

    dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 /*
 === Утилита cut ===
 
@@ -15,4 +23,53 @@ package main
 
 func main() {
 
+	var fields = flag.Int("f", -1, "выбрать поля (колонки)")
+	var delimiter = flag.String("d", "\t", "использовать другой разделитель")
+	var separated = flag.Bool("s", false, "только строки с разделителем")
+
+	log.SetFlags(0)
+	flag.Parse()
+
+	content, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	delimiterByte := []byte(*delimiter)
+	lines := bytes.Split(content, []byte("\n"))
+
+	if *separated {
+		separatedBytes(lines, delimiterByte)
+	}
+
+	delimiterBytes(lines, delimiterByte, fields)
+
+}
+
+func delimiterBytes(lines [][]byte, delimiterByte []byte, fields *int) {
+	res := make([][][]byte, 0)
+	for i := range lines {
+		temp := bytes.Split(lines[i], delimiterByte)
+		if field := *fields; field > -1 {
+			if len(temp) > field {
+				temp = [][]byte{temp[field]}
+			} else {
+				temp = [][]byte{}
+			}
+		}
+		res = append(res, temp)
+	}
+
+	fmt.Printf("%q", res)
+}
+
+func separatedBytes(lines [][]byte, delimiterByte []byte) [][]byte {
+	temp := make([][]byte, 0)
+	for i := range lines {
+		if bytes.Contains(lines[i], delimiterByte) {
+			temp = append(temp, lines[i])
+		}
+	}
+	lines = temp
+	return lines
 }
