14185f8e9b49933ecdd0cfa606a390cd80417a1b
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-23 23:14:09 +0300

    dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -34,5 +39,43 @@ fmt.Printf(“fone after %v”, time.Since(start))
 */
 
 func main() {
+	//var or func(channels ...<-chan interface{}) <-chan interface{}
+	//Пример использования функции:
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
 
+	test := merge(
+		sig(20*time.Second),
+		sig(50*time.Second),
+		sig(12*time.Second),
+		sig(6*time.Second),
+		sig(1*time.Second),
+	)
+	<-test
+	fmt.Printf("fone after %v", time.Since(start))
+}
+
+func merge(cs ...<-chan interface{}) <-chan interface{} {
+	test := make(chan interface{})
+	go func() {
+		for {
+			for i := range cs {
+				select {
+				case <-cs[i]:
+					close(test)
+					return
+				default:
+					continue
+				}
+			}
+		}
+	}()
+	return test
 }
