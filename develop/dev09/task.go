0309155fc696ac41f95c63a9d9e8aca367e538ad
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-23 22:22:32 +0300

    dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +16,25 @@ package main
 */
 
 func main() {
+	resp, err := http.Get(os.Args[1])
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("Use valid site")
+	}
 
+	recordFile("out.html", resp)
+}
+
+func recordFile(file string, m *http.Response) {
+	outFile, err := os.Create(file)
+	defer outFile.Close()
+	if err != nil {
+		log.Print("Cannot create file")
+	}
+
+	body, err := ioutil.ReadAll(m.Body)
+	if err != nil {
+		log.Println("Cannot read site body")
+	}
+	outFile.WriteString(string(body))
 }
