dcfa28345b8e8836a4617f30525b1b684c03c484
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-27 20:55:46 +0300

    dev08

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"github.com/mitchellh/go-ps"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -15,5 +24,80 @@ package main
 */
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for scanner.Scan() {
+		commandHandler(scanner.Text())
+	}
+
+}
+
+func commandHandler(stringCommand string) {
+
+	switch strings.Split(stringCommand, " ")[0] {
+
+	case "cd":
+		chDirCommand(stringCommand)
+
+	case "pwd":
+		pwdCommand()
+
+	case "echo":
+		echoCommand(stringCommand)
+
+	case "kill":
+		killPsCommand(stringCommand)
+
+	case "ps":
+		psCommand()
+
+	case `\quit`:
+		exitCommand()
+
+	default:
+		fmt.Println("Invalid command")
+	}
+}
+
+func chDirCommand(stringCommand string) {
+	err := os.Chdir(strings.Replace(stringCommand, "cd ", "", 1))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func pwdCommand() {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(dir)
+}
+
+func echoCommand(stringCommand string) {
+	fmt.Println(strings.Replace(stringCommand, "echo ", "", 1))
+}
+
+func killPsCommand(stringCommand string) {
+	pid, err := strconv.Atoi(strings.Replace(stringCommand, "kill ", "", 1))
+	if err != nil {
+		fmt.Println(err)
+	}
+	proc, err := os.FindProcess(pid)
+	if err != nil {
+		fmt.Println(err)
+	}
+	proc.Kill()
+}
+
+func psCommand() {
+	sliceProc, _ := ps.Processes()
+	for _, proc := range sliceProc {
+		fmt.Printf("Process name: %v process id: %v\n", proc.Executable(), proc.Pid())
+	}
+}
+
+func exitCommand() {
+	fmt.Println("Exit")
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/beevik/ntp v0.3.0 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
