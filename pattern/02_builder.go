3094ece0dcb4cfc5c49c8b672be09a498f0dfb97
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2022-03-28 23:08:09 +0300

    pattern facade

--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -5,3 +5,16 @@ package pattern
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+func main() {
+
+}
+
+type Human struct {
+	age      int
+	height   int
+	eyeColor string
+}
+
+func newHuman() Human {
+	return Human{}
+}
